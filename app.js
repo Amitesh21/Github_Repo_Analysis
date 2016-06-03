2db40fe9e1248f9a54177672cfe71e30edfe2953
Amitesh Jaiswal <amitesh.jaiswal21@gmail.com>
2016-06-03 16:42:51 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes');
+var repos = require('./routes/repos');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3000);
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
+app.get('/', routes.index);
+app.post('/dashboard', repos.dashboard);
+app.get('/dashboard', repos.redirectToMain);
+app.get('/listOfRepos', repos.listOfRepos);
+app.get('/commits', repos.commits);
+app.get('/getCommits', repos.getCommits);
+app.get('/userInformation',repos.userInformation);
+app.get('/getUserInformation', repos.getUserInformation);
+
+if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+module.exports = app;
